fd8725cc06020c181ac1dbd9069df2cbb73c355f
Renan Rodrigues <renangithub2002@gmail.com>
2026-02-23 20:08:07 -0300

    feat: initial project setup and test

---
+++ src/test/java/com/productionoptimizer/ProductionOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productionoptimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
